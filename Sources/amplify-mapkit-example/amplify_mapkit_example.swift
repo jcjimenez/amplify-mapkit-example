df6836af136cdcc3722a12128b04006e46bc6ae4
JC Jimenez <1117904+jcjimenez@users.noreply.github.com>
2023-03-29 11:31:58 -0500

    Blank spm package

---
+++ Sources/amplify-mapkit-example/amplify_mapkit_example.swift
@@ -0,0 +1,6 @@
+public struct amplify_mapkit_example {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/amplify-mapkit-exampleTests/amplify_mapkit_exampleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import amplify_mapkit_example
+
+final class amplify_mapkit_exampleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(amplify_mapkit_example().text, "Hello, World!")
+    }
+}
